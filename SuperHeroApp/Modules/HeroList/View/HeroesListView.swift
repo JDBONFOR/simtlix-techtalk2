499136c58ecc2555e0dd0bbb1e6f7fb5d39ab5ad
Juan Bonforti <juandaniel.bonforti@externos-cl.cencosud.com>
2021-09-07 17:25:47 -0300

    step 2

--- SuperHeroApp/Modules/HeroList/View/HeroesListView.swift
+++ SuperHeroApp/Modules/HeroList/View/HeroesListView.swift
@@ -5,31 +5,19 @@ struct HeroesListView: View {
     @ObservedObject var viewModel = HeroesViewModel()
     
     var body: some View {
-            
-        ZStack(alignment: .center) {
-            
-            if viewModel.showLoading {
-                LoaderView()
-            } else {
-                NavigationView {
-                    List {
-                        ForEach(viewModel.heroes, id: \.id) { hero in
-                            
-                            NavigationLink(destination: HeroDetailView(hero.id),
-                                           label: {
-                                            HeroListRowView(heroName: hero.name,
-                                                            heroImage: hero.images.lg)
-                                           })
-                        }
-                    }
-                    .navigationTitle("Characters")
-                }
+        NavigationView {
+            List {
+                HeroListRowView(heroName: "Personaje 1",
+                                heroImage: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/730-zatanna.jpg")
+                HeroListRowView(heroName: "Personaje 2",
+                                heroImage: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/730-zatanna.jpg")
+                HeroListRowView(heroName: "Personaje 3",
+                                heroImage: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/730-zatanna.jpg")
+                HeroListRowView(heroName: "Personaje 4",
+                                heroImage: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/730-zatanna.jpg")
             }
+            .navigationTitle("Characters")
         }
-        .onAppear {
-            viewModel.getAllHeroes()
-        }
-        
     }
 }
 
